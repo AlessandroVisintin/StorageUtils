c64b40eb0aaa7bd36b56ba830cd86fd7f6be4327
Alessandro Visintin <alevise.public@gmail.com>
2022-10-18 12:19:09 +0200

    Improved SQLite interface

--- StorageUtils/SQLite.py
+++ StorageUtils/SQLite.py
@@ -3,6 +3,7 @@ from JSONWrap.utils import load
 import sqlite3
 from pathlib import Path
 from typing import Iterable
+from typing import Any
 
 
 class SQLite:
@@ -13,46 +14,48 @@ class SQLite:
 	"""
 	
 	
-	def __init__(self, config_file:str) -> None:
+	def __init__(self, db_path:str, **kwargs) -> None:
 		"""
 		
 		Args:
-			config_file : configuration file path (YAML/JSON).
-				Syntax:
-					details :
-						name : str
-						location : str
-						same_thread : (true|false)
-					create :
-						table : query
-						...
-					insert :
-						table : query
-						...
+			db_path : path to database. Creates if not existing.
+			**kwargs :
+				same_thread (bool) : database connection used only
+					by a single thread. Defaults to True.
+				config_file : path to configuration file (YAML/JSON).
+					Syntax:
+						create :
+							- query
+							...
+						defaults :
+							name : query
+							...
 
 		Raises:
 			RuntimeError
  
 		"""
 		
-		self.cfg = load(config_file)
 		try:
-			name = self.cfg['details']['name']
-			location = self.cfg['details']['location']
-			thread = self.cfg['details']['same_thread']
-			Path(location).mkdir(parents=True,exist_ok=True)
-			self.db = sqlite3.connect(
-				f'{location}/{name}', check_same_thread=thread)
-			# configuring
+			self.cfg = load(kwargs['config_file'])
+			del kwargs['config_file']
+		except KeyError:
+			self.cfg = None
+		
+		Path(Path(db_path).parent).mkdir(parents=True,exist_ok=True)
+		self.db = sqlite3.connect(db_path, **kwargs)
+		
+		try:
 			with self.db as conn:
 				conn.execute('PRAGMA foreign_keys = ON;')
 			with self.db as conn:
-				for name,query in self.cfg['create'].items():
+				for query in self.cfg['create']:
 					conn.execute(query)
-		except KeyError:
-			raise RuntimeError('Missing mandatory key.')
 		except sqlite3.OperationalError as e:
 			raise RuntimeError(e)
+		except KeyError:
+			pass
+
 
 
 	def __del__(self):
@@ -67,21 +70,6 @@ class SQLite:
 			conn.execute('PRAGMA optimize;')
 		
 		self.db.close()
-	
-	
-	def add_query(self, group:str, key:str, query:str) -> None:
-		"""
-		
-		Add query to the config file.
-		
-		Args:
-			group : group where to add the query
-			key : name of the query
-			query : query to add
-		
-		"""
-		
-		self.cfg[group][key] = query
 	
 	
 	def schema(self) -> None:
@@ -115,40 +103,91 @@ class SQLite:
 			query = f'SELECT COUNT(*) AS count_{table_name} FROM {table_name};'
 			count = conn.execute(query).fetchall()[0][0]
 		return count
-			
-				
 
-	def insert(self, query_name, values:list[tuple]) -> None:
+
+	def execute(self, **kwargs) -> Iterable:
 		"""
 		
-		Insert rows in database.
+		Execute a query on the database.
 		
-		Args:
-			query_name : table where to insert.
-				Must match with key inside 'insert' in the config_file.
-			values : list of tuples with row values.
-
-		"""
-		
-		query = self.cfg['insert'][query_name]
-		with self.db as conn:
-			conn.executemany(query, values)
-
-	
-	def select(self, query_name, **kwargs) -> Iterable[tuple]:
-		"""
-		
-		Yields rows from database.
-		
-		Args:
-			query_name : select query to execute.
-				Must match with key inside 'select' in the config_file.
+		Args :
 			**kwargs :
-				parameters : list of parameters
-
+				name (str) : execute a default query from config_file.
+					It has precedence on 'query'.
+				query (str) : execute a custom query.
+				format (dict) : format query string with dictionary.
+				params (tuple or list[tuple]) : parameters for parametrized
+					query.
+		
+		Returns :
+			Yields result of query
+				
+		
 		"""
 		
-		query = self.cfg['select'][query_name]
+		query = None
+		if 'name' in kwargs:
+			try:
+				query = self.cfg['defaults'][kwargs['name']]
+			except KeyError:
+				raise RuntimeError(f'Unknwon default query {kwargs["name"]}')
+		elif 'query' in kwargs:
+			query = kwargs['query']
+		else:
+			raise RuntimeError('Execute requires a query')
+		
+		if 'format' in kwargs:
+			query = query.format(**kwargs['format'])
+		
 		with self.db as conn:
-			for row in conn.execute(query, **kwargs):
+			cur = None
+			try:
+				if isinstance(kwargs['params'], list):
+					cur = conn.executemany(query, kwargs['params'])
+				else:
+					cur = conn.execute(query, kwargs['params'])
+			except KeyError:
+				cur = conn.execute(query)
+			
+			for row in cur:
 				yield row
+				
+		
+		
+		
+		 
+
+# 	def insert(self, query_name, values:list[tuple]) -> None:
+# 		"""
+# 		
+# 		Insert rows in database.
+# 		
+# 		Args:
+# 			query_name : table where to insert.
+# 				Must match with key inside 'insert' in the config_file.
+# 			values : list of tuples with row values.
+
+# 		"""
+# 		
+# 		query = self.cfg['insert'][query_name]
+# 		with self.db as conn:
+# 			conn.executemany(query, values)
+
+# 	
+# 	def select(self, query_name, **kwargs) -> Iterable[tuple]:
+# 		"""
+# 		
+# 		Yields rows from database.
+# 		
+# 		Args:
+# 			query_name : select query to execute.
+# 				Must match with key inside 'select' in the config_file.
+# 			**kwargs :
+# 				parameters : list of parameters
+
+# 		"""
+# 		
+# 		query = self.cfg['select'][query_name]
+# 		with self.db as conn:
+# 			for row in conn.execute(query, **kwargs):
+# 				yield row
--- tests/SQLite_tests.py
+++ tests/SQLite_tests.py
@@ -1,25 +1,32 @@
 from StorageUtils.SQLite import SQLite
 
 
-# path to config file
+PATH = 'out/StorageUtils/test.db'
 CONFIG = 'config/StorageUtils/db_config.yaml'
 
 # open database
-db = SQLite(CONFIG)
+db = SQLite(PATH, config_file=CONFIG)
 
 # print schema
+print('Schema:')
 db.schema()
+
+# insert rows
+params = [
+	(1,'Mark','Twain',23),
+	(2,'Eloise','Thompson',34),
+	(3,'Madeleine','Water',27),
+	(4,'Alex','White',45),
+	(5,'Sam','Smith',18)
+]
+db.execute(name='insert_test', params=params)
 
 # count rows
 print('Number of rows: ', db.size('test'))
 
-# insert rows
-values = [
-	(1,'Mark','Twain',23),
-	(2,'Eloise','Thompson',34),
-	(3,'Madeleine','Water',27)
-	]
-db.insert('test', values)
-
 # get rows
-rows = [row for row in db.select('all_test')]
+rows = [row for row in db.execute(name='select_alltest')]
+
+# custom query
+query = 'SELECT * FROM test WHERE age < 30;'
+rows = [row for row in db.execute(query=query)]
